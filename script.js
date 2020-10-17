9a02dc6e1eb1ef422d13ac5bee36074f651ce2fc
alaa140101 <alaa140011@gmail.com>
2020-10-17 23:28:29 +0300

    Video Play/Pause

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const playSpeed = document.querySelector('.player-speed');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
+function showPlayBtn() {
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Play');
+}
 
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Pause');
+  }else {
+    video.pause();
+    showPlayBtn();
+  }
+}
 
+// Listener When video ended
+video.addEventListener('ended', showPlayBtn);
 
 // Progress Bar ---------------------------------- //
 
@@ -19,3 +45,6 @@
 // Fullscreen ------------------------------- //
 
 
+// Events Listener
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
